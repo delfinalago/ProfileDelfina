59d15c6a0e643b71146ea58affc718eb88147e02
delfinalago <delfinalago@gmail.com>
2021-03-18 22:57:43 -0300

    AboutMeV02

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,13 +3,26 @@ import {makeStyles} from '@material-ui/core'
 
 import Box from '@material-ui/core/Box';
 
+import logodel2 from '../logodel2.jpeg';
+
 
 const useStyles = makeStyles(theme => ({
 
-    boxAbout: {
-        justifyContent:"center",
-        display:"flex",
+    logo: {
+        margin: 'auto',
+        float:"left",
+        maxWidth: '100%',
+        maxHeight: '100%',
+        height: 250,
+        borderRadius: 15,
       },
+
+      parrafo: {
+        float:"left",
+        fontSize: 30,
+        padding: 15,
+      },
+
   }))
 
 
@@ -21,10 +34,17 @@ const AboutMe = () => {
     
     return(
 
-    <div style={{ width: '100%' }}>
-      <Box p={1}  className={classes.boxAbout}  bgcolor="secondary.main" color="primary.main" m={2}>
-        {"Hi ! My name is Delfina Lago im a Fullstack Developer. Dedicated and apasionate I seek every day to improve my skills in the IT world"}
-      </Box>
+    <div>
+         
+        <Box p={4} border={1} justifyContent="" display="flex" bgcolor="secondary.main" color="primary.main" m={4}>
+                <img src={logodel2} className={classes.logo} alt="logo" />   
+                <p className={classes.parrafo}> 
+                    Hi ! My name is Delfina Lago im a Fullstack Developer.
+                    <br/>
+                    Dedicated and apasionate I seek every day to improve my skills in the IT world.
+                </p>
+        </Box>
+
     </div>
      
     )
