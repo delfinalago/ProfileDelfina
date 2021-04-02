9d056a43b525a1378e8bf8ae8f6c597d84b8b67b
delfinalago <delfinalago@gmail.com>
2021-04-01 23:06:24 -0300

    Modifico Projects, mejoro grid (falta cargar proyectos y editar)

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,9 @@ import React from 'react';
 import Divider from '@material-ui/core/Divider';
 import {makeStyles} from '@material-ui/core/styles';
 
-import {Grid, Box, Button} from '@material-ui/core';
+import {Grid, Box} from '@material-ui/core';
+
+import Image from 'material-ui-image'
 
 import Veski from '../Imagenes/Veski.jpeg'
 
@@ -16,6 +18,27 @@ const useStyles = makeStyles(theme => ({
         textDecoration: "underline",
         fontFamily: "Poppins, sans-serif",
         fontWeight: 200,
+    },
+    DivProjects: {
+      margin: 100,  
+      textAlign: "center",
+
+    },
+    divImagen: {
+        
+        marginTop: 20,
+        marginLeft: 30,
+        marginRight: 30,
+        maxWidth: "100%",
+        
+    },
+    imagenprojects: {
+        
+        
+        borderRadius: 15,
+        
+        
+        
     }
 
 
@@ -29,46 +52,84 @@ const Projects = () => {
         <div>
 
             <h1 className={classes.tituloProjects}>PROJECTS</h1>
+            <div className={classes.DivProjects}>
+                <Grid container spacing={4}>
 
-            <Grid container spacing={2}>
+                
+                        <Grid item xs={12} sm={6} md={4} >
+                            <Box border={1} borderRadius={15}>
+                                <div className={classes.divImagen}>
+                                <Image className={classes.imagenprojects} src={Veski} />
+                                </div>
+                                <h1>
+                                Veski wallet
+                                </h1>
+                                <p>Skills Used: React Native,</p>
+                            </Box>
+                        </Grid>  
 
-                <Grid item xs={12} sm={6} md={4} >
-                    <Box border={2}>
-                        Veski wallet
-                    </Box>
-                </Grid>  
+                        <Grid item xs={12} sm={6} md={4} >
+                            <Box border={1} borderRadius={15}>
+                                <div className={classes.divImagen}>
+                                <Image className={classes.imagenprojects} src={Veski} />
+                                </div>
+                                <h1>
+                                Veski wallet
+                                </h1>
+                                <p>Skills Used: React Native,</p>
+                            </Box>
+                        </Grid>  
 
-                <Grid item xs={12} sm={6} md={4} >
-                    <Box border={2}>
-                        Veski wallet
-                    </Box>
-                </Grid>  
+                        <Grid item xs={12} sm={6} md={4} >
+                            <Box border={1} borderRadius={15}>
+                                <div className={classes.divImagen}>
+                                <Image className={classes.imagenprojects} src={Veski} />
+                                </div>
+                                <h1>
+                                Veski wallet
+                                </h1>
+                                <p>Skills Used: React Native,</p>
+                            </Box>
+                        </Grid>  
 
-                <Grid item xs={12} sm={6} md={4} >
-                    <Box border={2}>
-                        Veski wallet
-                    </Box>
-                </Grid>  
+                        <Grid item xs={12} sm={6} md={4} >
+                            <Box border={1} borderRadius={15}>
+                                <div className={classes.divImagen}>
+                                <Image className={classes.imagenprojects} src={Veski} />
+                                </div>
+                                <h1>
+                                Veski wallet
+                                </h1>
+                                <p>Skills Used: React Native,</p>
+                            </Box>
+                        </Grid>  
 
-                <Grid item xs={12} sm={6} md={4} >
-                    <Box border={2}>
-                        Veski wallet
-                    </Box>
-                </Grid>  
+                        <Grid item xs={12} sm={6} md={4} >
+                            <Box border={1} borderRadius={15}>
+                                <div className={classes.divImagen}>
+                                <Image className={classes.imagenprojects} src={Veski} />
+                                </div>
+                                <h1>
+                                Veski wallet
+                                </h1>
+                                <p>Skills Used: React Native,</p>
+                            </Box>
+                        </Grid>  
 
-                <Grid item xs={12} sm={6} md={4} >
-                    <Box border={2}>
-                        Veski wallet
-                    </Box>
-                </Grid>  
+                        <Grid item xs={12} sm={6} md={4} >
+                            <Box border={1} borderRadius={15}>
+                                <div className={classes.divImagen}>
+                                <Image className={classes.imagenprojects} src={Veski} />
+                                </div>
+                                <h1>
+                                Veski wallet
+                                </h1>
+                                <p>Skills Used: React Native,</p>
+                            </Box>
+                        </Grid>  
 
-                <Grid item xs={12} sm={6} md={4} >
-                    <Box border={2}>
-                        Veski wallet
-                    </Box>
-                </Grid>  
-
-            </Grid>
+                </Grid>
+            </div>
         </div>
     )
 
